994ba638a7b319325b8d132aa5967164bb10c58f
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2022-12-30 17:39:42 +0300

    implement the algoritm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (matrix) {
+        let newArr = matrix.map((el, i) => i % 2 != 0 ? el.reverse() : el);
+        return newArr.flat();
+    } else {
+        return [];
+    }
 }
